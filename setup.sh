cb8568f4ed44ec097ad23dd6dbf432ca278a883f
Aditya Sridhar <adilovesgh@users.noreply.github.com>
2020-09-24 12:26:48 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "Setting up necessary environment."
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential subversion cmake python3-dev
+sudo apt install libncurses5-dev libxml2-dev libedit-dev swig
+sudo apt install doxygen graphviz xz-utils git
